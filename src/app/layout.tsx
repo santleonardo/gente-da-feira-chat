fd72268ab5e333eab7e5351c8737827ee3f5b1f5
santleonardo <54089176+santleonardo@users.noreply.github.com>
2026-05-15 23:57:46 -0300

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export const viewport: Viewport = {
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
-  themeColor: "#01386A",
+  themeColor: "#0A4D5C",
 };
 
 export default function RootLayout({
